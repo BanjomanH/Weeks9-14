9fb5e82400d65c9a4952d4ce44ee855f1d99ee12
hickenb <hickenb@OA-S141-26>
2025-03-20 14:14:58 -0400

    Followed along with the in-class lecture. (Also added a timer start code that has some bugs)

--- Assets/Cuckoo Clock/KitClock.cs
+++ Assets/Cuckoo Clock/KitClock.cs
@@ -5,27 +5,58 @@ using UnityEngine.Events;
 
 public class KitClock : MonoBehaviour
 {
+    public Transform hourHand;
+    public Transform minuteHand;
     public float timeAnHourTakes = 5;
 
     public float t;
     public int hour = 0;
+    Coroutine clockIsRunning;
+    IEnumerator doOneHour;
 
-    public UnityEvent OnTheHour;
+    public UnityEvent <int> OnTheHour;
 
-    void Update()
+    void Start()
     {
-        t += Time.deltaTime;
+        clockIsRunning = StartCoroutine(MoveTheClock());
+    }
 
-        if (t > timeAnHourTakes)
+    IEnumerator MoveTheClock()
+    {
+        while (true)
         {
-            t = 0;
-            OnTheHour.Invoke();
-
-            hour++;
-            if (hour == 12)
-            {
-                hour = 0;
-            }
+            doOneHour = MoveTheClockHands1Hour();
+            yield return StartCoroutine(doOneHour);
         }
+    }
+
+    IEnumerator MoveTheClockHands1Hour()
+    {
+        t = 0;
+        while (t < timeAnHourTakes)
+        {
+            t += Time.deltaTime;
+            minuteHand.Rotate(0, 0, -(360/timeAnHourTakes * Time.deltaTime));
+            hourHand.Rotate(0, 0, -(30/timeAnHourTakes * Time.deltaTime));
+            yield return null;
+        }
+        hour++;
+        OnTheHour.Invoke(hour);
+    }
+
+    public void stopTheClock()
+    {
+        if (clockIsRunning != null)
+        {
+            StopCoroutine(clockIsRunning);
+        }
+        if (doOneHour != null)
+        {
+            StopCoroutine(doOneHour);
+        }
+    }
+    public void startTheClock()
+    {
+        clockIsRunning = StartCoroutine(MoveTheClock());
     }
 }
