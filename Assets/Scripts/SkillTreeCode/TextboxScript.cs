3ef367d4084fe0e8d6e48559d7cac4e5b52253c4
hickenb <hickenb@OA-S141-26>
2025-04-03 15:06:07 -0400

    Added the 'barText' code for properly displaying the state of the box and fixed a bug where you could hold down click to unlock something instantly.

--- Assets/Scripts/SkillTreeCode/TextboxScript.cs
+++ Assets/Scripts/SkillTreeCode/TextboxScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 using TMPro;
 using UnityEditor;
 using UnityEngine.Experimental.AI;
+using UnityEngine.Events;
 
 public class TextboxScript : MonoBehaviour
 {
@@ -14,13 +15,17 @@ public class TextboxScript : MonoBehaviour
     public AnimationCurve sizeCurve;
     public TMP_Text Title;
     public TMP_Text Body;
+    public TMP_Text barText;
     public Slider bar;
+    public GameObject barVisuals;
+    public string[] barResponses;
 
     bool createBoxRunning = false;
     float time = 0;
 
     public void newTextBox(GameObject nodeToSelect)
     {
+        bar.value = 0;
         selectedNode = nodeToSelect;
         NodeCode temp = nodeToSelect.GetComponent<NodeCode>();
         Title.text = temp.title;
@@ -49,18 +54,26 @@ public class TextboxScript : MonoBehaviour
                 time += Time.deltaTime * 3;
                 transform.localScale = Vector3.one * sizeCurve.Evaluate(time);
             }
-            
+
+            barText.text = barResponses[selectedNode.GetComponent<NodeCode>().state];
             if (Input.GetMouseButton(0) && selectedNode.GetComponent<NodeCode>().state == 1)
             {
+                barVisuals.SetActive(true);
                 bar.value += Time.deltaTime;
                 if (bar.value == 1)
                 {
                     selectedNode.GetComponent<NodeCode>().Unlock();
                 }
+            } else if (selectedNode.GetComponent<NodeCode>().state == 2)
+            {
+                bar.value = 1;
+                barVisuals.SetActive(true);
             } else
             {
                 bar.value = 0;
+                barVisuals.SetActive(false);
             }
+
             yield return null;
         }
     }
