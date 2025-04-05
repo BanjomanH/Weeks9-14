aa35434134aa12c3a34288ef460ed55df709eee3
BanjomanH <hickenb@sheridancollege.ca>
2025-04-05 18:01:36 -0400

    Added comments to all of the code

--- Assets/Scripts/SkillTreeCode/NodeCode.cs
+++ Assets/Scripts/SkillTreeCode/NodeCode.cs
@@ -6,25 +6,27 @@ using UnityEngine.UI;
 
 public class NodeCode : MonoBehaviour
 {
-    public GameObject[] futureNodes;
-    public GameObject textbox;
-    public AnimationCurve sizeCurve;
-    public UnityEvent becomeSelectable;
-    public Sprite[] iconFiles;
-    public Image icon;
-    public Material lineMaterial;
-    public Material purchasedMaterial;
-    public Material purchasableMaterial;
-    public int state = 0;
-    public string title;
-    public string body;
+    public GameObject[] futureNodes; // An array containing all of the nodes ahead of this one
+    public GameObject textbox; // A reference to the textbox gameObject
+    public AnimationCurve sizeCurve; // An animation curve for the unlocking animation
+    public UnityEvent becomeSelectable; // calls the selectable method in the next gameObjects
+    public Sprite[] iconFiles; // An array containing the possible icons to display
+    public Image icon; // A reference to the child objects Image component
+    public Material lineMaterial; // The material of a locked link/node
+    public Material purchasedMaterial; // The material of a purchased link/node
+    public Material purchasableMaterial; // The material of a standard link
+    public int state = 0; // The current state (0 = locked, 1 = purchasable, 2 = purchased)
+    public string title; // The title of the skill to send to the textbox
+    public string body; // The description of the skill to send to the textbox
 
-    Image background;
+    Image background; // A reference to the current background of the gameObject
 
+    // Code runs on startup
     void Start()
     {
-        background = GetComponent<Image>();
+        background = GetComponent<Image>(); // Properly references the background variable
 
+        // The following sets the look to the correct state. Mostly for the first node.
         if (state == 0)
         {
             background.material = lineMaterial;
@@ -32,8 +34,9 @@ public class NodeCode : MonoBehaviour
         {
             background.material = null;
         }
+        icon.sprite = iconFiles[state];
 
-            icon.sprite = iconFiles[state];
+        // Iterates through the futureNodes array to create lines and subscribe them to the becomeSelectable event
         for (int i = 0; i < futureNodes.Length; i++)
         {
             futureNodes[i].GetComponent<NodeCode>().drawLine(transform.position);
@@ -45,10 +48,12 @@ public class NodeCode : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // Iterates through the futureNodes array, testing to see if any of them have been purchased
         for (int i = 0; i < futureNodes.Length; i++)
         {
             if (futureNodes[i].GetComponent<NodeCode>().state == 2)
             {
+                // If so, gets the lineRenderer to change its visuals to be thicker and a different material
                 futureNodes[i].GetComponent<LineRenderer>().startWidth = 0.5f;
                 futureNodes[i].GetComponent<LineRenderer>().endWidth = 0.5f;
                 futureNodes[i].GetComponent<LineRenderer>().material = purchasedMaterial;
@@ -56,47 +61,62 @@ public class NodeCode : MonoBehaviour
         }
     }
 
+    // Called from the textbox that holds a reference to this object
     public void Unlock()
     {
+        // Changes the state of the node, and updates it's visuals accordingly
         state = 2;
         background.material = purchasedMaterial;
         icon.sprite = iconFiles[state];
+        // Invokes the becomeSelectable event to activate the next nodes
         becomeSelectable.Invoke();
     }
 
+    // Called from previous nodes who has a reference to this one
     public void Selectable()
     {
+        // Changes the state of the node, and updates it's visuals accordingly
         state = 1;
         background.material = null;
         icon.sprite = iconFiles[state];
         GetComponent<LineRenderer>().material = purchasableMaterial;
+        // Begins the cosmetic coroutine of this node unlocking
         StartCoroutine(GrowThenShrink());
     }
 
+    // Referenced with the previous nodes position. Called from the previous node.
     public void drawLine(Vector3 pastNode)
     {
+        // Arranges the variables to have that offset effect over the node
         Vector3 currentPos = transform.position;
         currentPos.z = -0.7f;
         pastNode.z = 1;
         LineRenderer temp = gameObject.GetComponent<LineRenderer>();
 
+        // Sets the visuals to the correct size and material
         temp.material = lineMaterial;
         temp.startWidth = 0.3f;
         temp.endWidth = 0.3f;
 
+        // Sets the positions to 2, and sets the two position variables accordingly.
         temp.positionCount = 2;
         temp.SetPosition(0, pastNode);
         temp.SetPosition(1, currentPos);
     }
 
+    // Called from a ‘pointer enter’ unity trigger over this current node
     public void hover()
     {
+        // Calls the method to create a new textBox, with itself as the reference
         textbox.GetComponent<TextboxScript>().newTextBox(gameObject);
     }
 
+    // Called from the 'selectable method'
     IEnumerator GrowThenShrink()
     {
+        // Create a temporary time float
         float t = 0;
+        // before a second, set the localScale to be the sizeCurve according to the time float
         while (t < 1)
         {
             t += Time.deltaTime;
--- Assets/Scripts/SkillTreeCode/TextboxScript.cs
+++ Assets/Scripts/SkillTreeCode/TextboxScript.cs
@@ -10,66 +10,82 @@ using UnityEngine.Events;
 
 public class TextboxScript : MonoBehaviour
 {
-    public GameObject selectedNode;
-    public Coroutine createBox;
-    public AnimationCurve sizeCurve;
-    public TMP_Text Title;
-    public TMP_Text Body;
-    public TMP_Text barText;
-    public Slider bar;
-    public GameObject barVisuals;
-    public string[] barResponses;
+    public GameObject selectedNode; // A reference to the latest node the player is hovering over
+    public Coroutine createBox; //The coroutine responsible for managing the text box.
+    public AnimationCurve sizeCurve; // An animation curve for the growing/shrinking animation
+    public TMP_Text Title; // A reference to the 'title' gameObject contained on the gameObjects child.
+    public TMP_Text Body; // A reference to the 'body' gameObject contained on the gameObjects child.
+    public TMP_Text barText; // A reference to the text gameObject overlaid on the slider.
+    public Slider bar; // A reference to the bar contained on this gameObjects child.
+    public GameObject barVisuals; // A reference to the bar fill gameObject
+    public string[] barResponses; // A list of the possible texts overlayed on the bar
 
-    bool createBoxRunning = false;
-    float time = 0;
+    bool createBoxRunning = false; // A boolean that turns on when the 'createBox' coroutine is running
+    float time = 0; // An inputted number into the sizeCurve for smooth transitions
 
+    // Called from the nodes 'hover' unityEvent
     public void newTextBox(GameObject nodeToSelect)
     {
-        bar.value = 0;
+        // Sets the inputted gameObject to be the next node to select
         selectedNode = nodeToSelect;
+        // Arranges all of the visuals accordingly
+        bar.value = 0;
         NodeCode temp = nodeToSelect.GetComponent<NodeCode>();
         Title.text = temp.title;
         Body.text = temp.body;
         GetComponentInChildren<SpriteRenderer>().sprite = temp.iconFiles[1];
+        // Sets the positions of the textBox to be offset from the normal
         Vector3 mousePosition = nodeToSelect.transform.position;
         mousePosition.z = -3;
         mousePosition.y += 2.2f;
         transform.position = mousePosition;
         
+        // If any textBox is currently active, then stop them
         if (createBox != null)
         {
             StopCoroutine(createBox);
         }
+        // Start a new textbox with the 'createBox' coroutine
         createBox = StartCoroutine(CreateBox());
     }
 
+    // The code responsible for running the textBox function
     IEnumerator CreateBox()
     {
         time = 0;
         createBoxRunning = true;
+        // Runs forever. Only stopped by the 'exitNode' method
         while (true)
         {
+            // before a second, set the localScale to be the sizeCurve according to the time float
             if (time < 1)
             {
                 time += Time.deltaTime * 3;
                 transform.localScale = Vector3.one * sizeCurve.Evaluate(time);
             }
 
+            // All of the code responsible for ensuring the bar works properly
+            // Sets the correct response based on the state of the hovered node
             barText.text = barResponses[selectedNode.GetComponent<NodeCode>().state];
+            // If the player clicks while hovering over a selectable node...
             if (Input.GetMouseButton(0) && selectedNode.GetComponent<NodeCode>().state == 1)
             {
+                // Enable the visuals and begin to increment the sliders value
                 barVisuals.SetActive(true);
                 bar.value += Time.deltaTime;
                 if (bar.value == 1)
                 {
+                    // Calls the unlock script on the hovered over node
                     selectedNode.GetComponent<NodeCode>().Unlock();
                 }
             } else if (selectedNode.GetComponent<NodeCode>().state == 2)
             {
+                // If the hovered over node is unlocked, display the unlocked visuals
                 bar.value = 1;
                 barVisuals.SetActive(true);
             } else
             {
+                // If the hovered over node is locked, display the locked visuals
                 bar.value = 0;
                 barVisuals.SetActive(false);
             }
@@ -78,20 +94,27 @@ public class TextboxScript : MonoBehaviour
         }
     }
 
+    // The script for closing the box
     IEnumerator BoxEnd()
     {
+        // If time hasn't hit zero yet and the createBox coroutine is not running...
         while (time > 0 && createBoxRunning == false)
         {
+            // Grabs whatever the time value was set to and begins to decrement it
             time -= Time.deltaTime * 3;
+            // Sets the localScale to be the sizeCurve according to the time float
             transform.localScale = Vector3.one * sizeCurve.Evaluate(time);
             yield return null;
         }
     }
 
+    // Called from a nodes ‘pointer exit’ unity event.
     public void exitNode()
     {
+        // Stops whatever coroutine was running and sets the corrosponding variable to false
         StopCoroutine(createBox);
         createBoxRunning = false;
+        // Begins the shrinking coroutine
         StartCoroutine(BoxEnd());
     }
 }
